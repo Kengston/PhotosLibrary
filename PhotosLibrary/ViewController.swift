080625acf9e84188a5dec26a72eacbbca62ea23f
Danil Lysikov <danildc@mail.ru>
2020-05-03 12:21:09 +0700

    release v

---
+++ PhotosLibrary/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PhotosLibrary
+//
+//  Created by Данил Лысиков on 02.04.2020.
+//  Copyright © 2020 Данил Лысиков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .yellow
+    }
+
+}
+
